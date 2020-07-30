93420246afb93aaa4bf324cee1726f3c246863c3
Piotr Stanny <piotr.stanny@gmail.com>
2020-07-30 23:36:02 +0100

    Implement containers for contact details

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,23 +10,78 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.white,
+        backgroundColor: Colors.blueGrey,
         body: SafeArea(
-          child: Column(
+          child: Row(
             mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
             children: <Widget>[
-              CircleAvatar(
-                radius: 100.0,
-                backgroundImage: AssetImage('assets/avatar.jpg'),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  CircleAvatar(
+                    radius: 100.0,
+                    backgroundImage: AssetImage('assets/avatar.jpg'),
+                  ),
+                  Text(
+                    'Piotr Stanny',
+                    style: GoogleFonts.pacifico(
+                      fontSize: 40.0,
+                    ),
+                  ),
+                  Text(
+                    'FLUTTER DEVELOPER',
+                    style: GoogleFonts.sourceCodePro(
+                      fontSize: 25.0,
+                      fontWeight: FontWeight.bold,
+                      letterSpacing: 2.5,
+                      color: Colors.blueGrey[800],
+                    ),
+                  ),
+//                  Card(
+//                    margin: EdgeInsets.symmetric(vertical: 40.0),
+//                    color: Color(0xFFFF5722),
+//                    child: Padding(
+//                      padding: EdgeInsets.all(15.0),
+//                      child: ListTile(
+//                        leading: Icon(
+//                          Icons.phone,
+//                          size: 30.0,
+//                          color: Colors.white,
+//                        ),
+//                        title: Text('+44 7559 616 468'),
+//                      ),
+//                    ),
+//                  ),
+                  Container(
+                    padding: EdgeInsets.symmetric(vertical: 15.0),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(6.0),
+                      color: Color(0xFFFF5722),
+                    ),
+                    width: 350.0,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Icon(
+                          Icons.email,
+                          size: 30.0,
+                          color: Colors.white,
+                        ),
+                        SizedBox(
+                          width: 30.0,
+                        ),
+                        Text(
+                          'piotr.stanny@gmail.com',
+                          style: TextStyle(
+                            fontSize: 20.0,
+                            color: Colors.white,
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ],
               ),
-              Text(
-                'Piotr Stanny',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 30.0,
-                ),
-              )
             ],
           ),
         ),
