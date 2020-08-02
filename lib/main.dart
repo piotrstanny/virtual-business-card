392755a080b57217b78e876f3ef5b31c32090d08
Piotr Stanny <piotr.stanny@gmail.com>
2020-08-02 14:15:44 +0100

    Use Card and ListTile components for contact details

--- lib/main.dart
+++ lib/main.dart
@@ -12,75 +12,70 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.blueGrey,
         body: SafeArea(
-          child: Row(
+          child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  CircleAvatar(
-                    radius: 100.0,
-                    backgroundImage: AssetImage('assets/avatar.jpg'),
+              CircleAvatar(
+                radius: 100.0,
+                backgroundImage: AssetImage('assets/avatar.jpg'),
+              ),
+              Text(
+                'Piotr Stanny',
+                style: GoogleFonts.pacifico(
+                  fontSize: 40.0,
+                ),
+              ),
+              Text(
+                'FLUTTER DEVELOPER',
+                style: GoogleFonts.sourceCodePro(
+                  fontSize: 25.0,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 2.5,
+                  color: Colors.blueGrey[800],
+                ),
+              ),
+              SizedBox(
+                height: 40.0,
+                width: 150.0,
+                child: Divider(
+                  color: Colors.amber,
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.fromLTRB(20.0, 0, 20.0, 20.0),
+                color: Colors.amber[400],
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    size: 30.0,
+                    color: Colors.blueGrey[800],
                   ),
-                  Text(
-                    'Piotr Stanny',
-                    style: GoogleFonts.pacifico(
-                      fontSize: 40.0,
-                    ),
-                  ),
-                  Text(
-                    'FLUTTER DEVELOPER',
-                    style: GoogleFonts.sourceCodePro(
-                      fontSize: 25.0,
-                      fontWeight: FontWeight.bold,
-                      letterSpacing: 2.5,
+                  title: Text(
+                    'piotr.stanny@gmail.com',
+                    style: TextStyle(
+                      fontSize: 20.0,
                       color: Colors.blueGrey[800],
                     ),
                   ),
-//                  Card(
-//                    margin: EdgeInsets.symmetric(vertical: 40.0),
-//                    color: Color(0xFFFF5722),
-//                    child: Padding(
-//                      padding: EdgeInsets.all(15.0),
-//                      child: ListTile(
-//                        leading: Icon(
-//                          Icons.phone,
-//                          size: 30.0,
-//                          color: Colors.white,
-//                        ),
-//                        title: Text('+44 7559 616 468'),
-//                      ),
-//                    ),
-//                  ),
-                  Container(
-                    padding: EdgeInsets.symmetric(vertical: 15.0),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(6.0),
-                      color: Color(0xFFFF5722),
-                    ),
-                    width: 350.0,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Icon(
-                          Icons.email,
-                          size: 30.0,
-                          color: Colors.white,
-                        ),
-                        SizedBox(
-                          width: 30.0,
-                        ),
-                        Text(
-                          'piotr.stanny@gmail.com',
-                          style: TextStyle(
-                            fontSize: 20.0,
-                            color: Colors.white,
-                          ),
-                        )
-                      ],
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(horizontal: 20.0),
+                color: Colors.amber[400],
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    size: 30.0,
+                    color: Colors.blueGrey[800],
+                  ),
+                  title: Text(
+                    '+44 7459 616 468',
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      color: Colors.blueGrey[800],
                     ),
                   ),
-                ],
+                ),
               ),
             ],
           ),
